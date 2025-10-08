00b2c063073d4e411017515803b6e420509d98cc
Qiute <qiuterry3007@gmail.com>
2025-10-08 08:53:30 +0200

    06

--- film.php
+++ film.php
@@ -6,15 +6,19 @@ class film{
     private realisateur $sonRealisateur;
     private array $lesActeurs = [];
     
-    public function __construct(int $idFilm, string $nomFilm, int $nbEntrees, realisateur $sonRealisateur)
+    public function __construct(int $idFilm, string $nomFilm, int $nbEntrees, Realisateur $realisateur, array $acteurs)
     {
         $this->idFilm = $idFilm;
         $this->nomFilm = $nomFilm;
-        $this->nbEntrees = $nbEntrees; 
-        $this->sonRealisateur = $sonRealisateur; 
-        $this->lesActeurs= $lesActeurs;
+        $this->nbEntrees = $nbEntrees;
+        $this->realisateur = $realisateur;
+        $this->acteurs = $acteurs;
     }
 
+    public function getActeurs(): array
+    {
+        return $this->acteurs;
+    }
     public function getIdFilm(): int
     {
         return $this->idFilm;
